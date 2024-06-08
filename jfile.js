b5ad004cdb889d5d30120f79860c8e51c161a53f
meghana nenta <115071105+B191130@users.noreply.github.com>
2024-06-08 17:10:01 +0530

    Create jfile.js

---
+++ jfile.js
@@ -0,0 +1,39 @@
+// Function to fetch weather data from the API based on user input
+function searchWeather() {
+    const cityName = document.getElementById("city-input").value.trim();
+    if (cityName) {
+        fetchWeatherData(cityName);
+    } else {
+        alert("Please enter a city name");
+    }
+}
+
+// Function to fetch weather data from the API
+function fetchWeatherData(cityName) {
+    const apiKey = 'd07624d94b339e686f4573049e57c20c'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const weatherData = {
+                city: data.name,
+                temperature: `${data.main.temp}°C`
+            };
+            updateWeatherInfo(weatherData);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+// Function to update weather information on the page
+function updateWeatherInfo(data) {
+    document.getElementById("city-name").textContent = data.city;
+    document.getElementById("temperature").textContent = data.temperature;
+}
